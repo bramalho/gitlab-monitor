dd1d64b9dcd94c61030758d5b1314d8b08999cef
Bruno Ramalho <bfmramalho@gmail.com>
2019-10-20 11:05:11 +0200

    get projects

--- project.go
+++ project.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"strings"
 
 	"github.com/xanzy/go-gitlab"
 )
 
-// GetProjectData from projects
-func GetProjectData(c *gitlab.Client, pid string) {
-	project, err := GetProject(c, pid)
-	if err == nil {
-		fmt.Println(project.NameWithNamespace)
-		fmt.Println(project.AvatarURL)
+// Project struct
+type Project struct {
+	ID     string
+	Name   string
+	Avatar string
+}
+
+func getProjetIds() []string {
+	projects := []string{}
+	if data, exists := os.LookupEnv("PROJECTS"); exists {
+		result := strings.Split(data, ",")
+		for i := range result {
+			if len(result[i]) > 0 {
+				projects = append(projects, result[i])
+			}
+		}
 	}
+
+	return projects
+}
+
+// GetProjectData from projects
+func GetProjectData(c *gitlab.Client) []Project {
+	projects := []Project{}
+
+	for _, pid := range getProjetIds() {
+		project, err := GetProject(c, pid)
+		if err == nil {
+			projects = append(projects, Project{
+				ID:     pid,
+				Name:   project.NameWithNamespace,
+				Avatar: project.AvatarURL,
+			})
+		}
+	}
+
+	return projects
 }
